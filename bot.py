0dbcfb51bd3c729b573596280055ce33a5d0ff8f
Youssef Ahmed <yousseffahmed@hotmail.com>
2019-07-21 02:54:10 +0200

    adding version one Pyhthon1 - feature 1.1

---
+++ bot.py
@@ -0,0 +1,23 @@
+import os.path
+from rivescript import RiveScript
+
+
+file = os.path.dirname(__file__)
+brain = os.path.join(file,'brain')
+
+
+bot = RiveScript(utf8=True) # enables Arabic
+bot.load_directory(brain)
+bot.sort_replies()
+
+
+while True :
+    msg = str(input('You> '))
+
+    if msg == 'q':
+        break
+
+    else:
+       print('Bot> ' + bot.reply('localuser',msg))
+
+
